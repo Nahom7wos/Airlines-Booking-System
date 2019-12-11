3ee35195ae144eaf54ff7519c54f0161ebc40ed7
Nahom7wos <nahom7wos@gmail.com>
2019-12-11 20:40:40 +0300

    hanler

--- airlines/http/main.go
+++ airlines/http/main.go
@@ -1,6 +1,24 @@
+package main
 
-// import menu hadler
+import (
+	"html/template"
+	"net/http"
+	// import menu hadler
+)
 
 // connect database - previleges?
 
-// handlefunc, start server
+// handlefunc, start server, (can't declare := outsite func)
+var tmpl = template.Must(template.ParseGlob("../../ui/templates/*"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "index.layout", nil)
+}
+func main() {
+	fs := http.FileServer(http.Dir("../../ui/assets"))
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/", index)
+	http.ListenAndServe(":8080", mux)
+}
