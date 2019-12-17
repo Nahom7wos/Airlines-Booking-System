6fbc9f896b9ebb3447ce9107b25bf05372507387
ElbethelSolomon5149 <58727759+ElbethelSolomon5149@users.noreply.github.com>
2019-12-17 11:48:49 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	// import menu hadler
+)
+
+// connect database - previleges?
+
+// handlefunc, start server
+var tmpl = template.Must(template.ParseGlob("../../ui/templates/*"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "index.layout", nil)
+}
+func book(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "book.layout", nil)
+}
+func checkin(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "checkin.layout", nil)
+}
+func main() {
+	fs := http.FileServer(http.Dir("../../ui/assets"))
+	mux := http.NewServeMux()
+	mux.Handle("/ui/assets/", http.StripPrefix("../../ui/assets/", fs))
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/book", book)
+	mux.HandleFunc("/checkin", book)
+	http.ListenAndServe(":8080", mux)
+}
