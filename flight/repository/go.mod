6c1f9134f14eaae6598179a6b2099c966f60954d
Nahom7wos <nahom7wos@gmail.com>
2020-01-11 21:30:40 +0300

    Revert "Merge branch 'master' of https://github.com/Nahom7wos/Airlines-Booking-System"
    This reverts commit 5dff7c87d8634f22817c1972246327c6c3ef7e75, reversing
    changes made to 5aa9feb8dfcaef27ed7187caf584750c300b5815.

---
+++ flight/repository/go.mod
@@ -0,0 +1,3 @@
+module github.com/Nahom7wos/Airlines-Booking-System/flight/repository
+
+go 1.13
---
+++ flight/service/destination_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"github.com/Nahom7wos/Airlines-Booking-System/entity"
+	"github.com/Nahom7wos/Airlines-Booking-System/flight"
+)
+
+// DestinationService implements flight.DestinationService interface
+type DestinationService struct {
+	destinationRepo flight.DestinationRepository
+}
+
+// NewDestinationService will create new DestinationService object
+func NewDestinationService(destRepo flight.DestinationRepository) flight.DestinationService {
+	return &DestinationService{destinationRepo: destRepo}
+}
+
+// Destinations returns list of destinations
+func (dServ *DestinationService) Destinations() ([]entity.Destination, []error) {
+
+	destinations, errs := dServ.destinationRepo.Destinations()
+
+	if len(errs) > 0 {
+		return nil, errs
+	}
+
+	return destinations, nil
+}
+
+// StoreDestination persists new destination information
+func (dServ *DestinationService) StoreDestination(destination *entity.Destination) (*entity.Destination, []error) {
+
+	dstn, errs := dServ.destinationRepo.StoreDestination(destination)
+
+	if len(errs) > 0 {
+		return nil, errs
+	}
+
+	return dstn, nil
+}
